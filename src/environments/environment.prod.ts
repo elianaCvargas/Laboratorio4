6f806b4abedaaefc04ec84512c83b5f0b80035d0
Eliana <carla.e.vargas@gmail.com>
2022-04-29 22:18:07 -0300

    conexion a firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,13 @@
 export const environment = {
-  production: true
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyBmm3dVk57cIE386P2In7MTLsSZ9y_6Qu8",
+    authDomain: "saladejuegos-app.firebaseapp.com",
+    databaseURL: "https://saladejuegos-app.firebaseio.com",
+    projectId: "saladejuegos-app",
+    storageBucket: "saladejuegos-app.appspot.com",
+    messagingSenderId: "718871633595",
+    appId: "1:718871633595:web:406fb238f52b438dc6139e",
+    measurementId: "G-PESMHEC2TB"
+  }
 };
-
